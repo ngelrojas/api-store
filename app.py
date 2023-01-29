c3fa309e4882f9f6baca759b53533efef1c9e3d5
ngel rojas <aomarrojasp@gmail.com>
2023-01-29 12:00:17 -0300

    error: this branch is an error

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, db, render_as_batch=True)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "rednodes"
