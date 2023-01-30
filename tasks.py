00b7fc3a8b55f7d17c8c38f56e6c7438996a539d
ngel rojas <aomarrojasp@gmail.com>
2023-01-30 12:03:42 -0300

    add: adding html templates, templates directory and setup to send with email temaplete

--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
 import os
+
+import jinja2
 import requests
 from dotenv import load_dotenv
 
@@ -7,8 +9,15 @@ load_dotenv()
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
 api_key = os.getenv("MAILGUN_API_KEY")
 
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
         auth=("api", api_key),
@@ -16,7 +25,8 @@ def send_simple_message(to, subject, body):
             "from": f"info rednodes <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body
+            "text": body,
+            "html": html
         }
     )
 
@@ -25,5 +35,6 @@ def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "successfully signed up",
-        f"hi {username}! you have successfully signed up to the stores rest api."
+        f"hi {username}! you have successfully signed up to the stores rest api.",
+        render_template("email/action.html", username=username)
     )
